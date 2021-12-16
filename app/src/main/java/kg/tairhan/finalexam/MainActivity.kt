8326e10ef096343d7a708bbc40b59cb5a3850be5
osmonaliev_b@auca.kg <Beak2506>
2021-12-16 15:09:54 +0600

    AlimFinal

--- app/src/main/java/kg/tairhan/finalexam/MainActivity.kt
+++ app/src/main/java/kg/tairhan/finalexam/MainActivity.kt
@@ -1,11 +1,11 @@
-package kg.tairhan.finalexam
+package kg.khalilov.finalexam;
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
+import android.app.Activity
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
     }
 }
